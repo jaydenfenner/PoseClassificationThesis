d2df99129c7e1392957e36e3fd75ee9a23bb36e7
Jayden Fenner <jaydenfen@gmail.com>
2024-09-09 13:58:49 +0800

    working: removed unused main.py code from runInference.py

--- runInference.py
+++ runInference.py
@@ -10,6 +10,7 @@ IMPORTANT NOTES
 
 from data.SLP_RD import SLP_RD
 from data.SLP_FD import SLP_FD
+from main import validate
 import utils.vis as vis
 import utils.utils as ut
 import numpy as np
@@ -96,6 +97,19 @@ with open(pth_rst, 'w') as f:
 
 # arguments?
 opts = opt.parseArgs()
+######################################################## MY ARGUMENTS
+pretrained_model_name = "SLP_depth_u12_HRpose_exp" # name of the pretrained model folder
+
+# parser.add_argument('--SLP_set', default='danaLab', help='[danaLab|simLab] for SLP section')
+opts.SLP_set = 'simLab'
+
+# parser.add_argument('--mod_src', nargs='+', default=['IR'],
+#     help='source modality list, can accept multiple modalities typical model [RGB|IR|depthRaw| PMarray]')
+opts.mod_src = ['depth']
+
+# parser.add_argument('--cov_li', nargs='+', default=['uncover', 'cover1', 'cover2'], help='the cover conditions')
+opts.cov_li = ['cover2']
+########################################################
 if 'depth' in opts.mod_src[0]:  # the leading modalities, only depth use tight bb other raw image size
     opts.if_bb = True  # not using bb, give ori directly
 else:
@@ -104,33 +118,10 @@ exec('from model.{} import get_pose_net'.format(opts.model))  # pose net in
 opts = opt.aug_opts(opts)  # add necesary opts parameters   to it
 
 
-
-####################
-#### MY ARGUMENTS
-mod_src = ['depth'] # ['depth', 'IR', 'RGB', 'PM'] (I think)
-####################
-
+############################################################################################################
 # run inference (work in progress)
-rst_test = validate(
-        test_loader, SLP_rd_test, model, criterion,
-        n_iter=n_iter, logger=logger, opts=opts, if_svVis=True)
+############################################################################################################
 def main():
-    ############################################### my additions START ###################
-    #### my additions:
-
-    # calc number of channels based on the set of source modalities
-    MY_input_nc = 0
-    for mod in mod_src:
-        if 'RGB' in mod:
-            input_nc+=3
-        else:
-            input_nc += 1
-
-    # name of the pretrained model folder
-    pretrained_model_name = "SLP_depth_u12_HRpose_exp"
-    ############################################### my additions END ###################
-
-
     # logger setup (can't hurt surely)
     if_test = opts.if_test
     if if_test:
@@ -144,13 +135,14 @@ def main():
     n_jt = SLP_RD.joint_num_ori
 
     # get model (architecture only)
-    model = get_pose_net(in_ch=MY_input_nc, out_ch=n_jt) # prev opts.input_nc
+    model = get_pose_net(in_ch=opts.input_nc, out_ch=n_jt) # prev opts.input_nc
 
     # load in pretrained weights and initialise model ###########################################################################
     pretrained_model_path = os.path.join("pretrainedModels", pretrained_model_name, 'model_dump/checkpoint.pth')
     logger.info("=> loading checkpoint '{}'".format(pretrained_model_path))
-    checkpoint = torch.load(pretrained_model_path)
+    checkpoint = torch.load(pretrained_model_path, map_location=torch.device('cpu')) ######################### note extra argument to use cpu
     model.load_state_dict(checkpoint['state_dict'])
+    model = torch.nn.DataParallel(model, device_ids=opts.gpu_ids).cpu() # paralellise the torch operations
 
     # define loss function (criterion) and optimizer
     criterion = JointsMSELoss(      # try to not use weights
@@ -164,100 +156,8 @@ def main():
     test_loader = DataLoader(dataset=SLP_fd_test, batch_size = opts.batch_size // len(opts.trainset),
                               shuffle=False, num_workers=opts.n_thread, pin_memory=opts.if_pinMem)
 
-    # get optmizer
-    # best_perf = 0.0
-    # last_epoch = -1
-    # optimizer = Adam(model.parameters(), lr=opts.lr)
-    # checkpoint_file = os.path.join(
-    #     opts.model_dir, 'checkpoint.pth')
-    # if 0 == opts.start_epoch or not path.exists(checkpoint_file):  #    from scratch
-    #     begin_epoch =  0     # either set or not exist all the same from 0
-    #     losses = []     # for tracking model performance.
-    #     accs= []
-    # else:  # get chk points
-    #     logger.info("=> loading checkpoint '{}'".format(checkpoint_file))
-    #     checkpoint = torch.load(checkpoint_file)
-    #     # begin_epoch = checkpoint['epoch']
-    #     # best_perf = checkpoint['perf']
-    #     # last_epoch = checkpoint['epoch']
-    #     model.load_state_dict(checkpoint['state_dict'])  # here should be cuda setting
-        # losses = checkpoint['losses']
-        # accs = checkpoint['accs']
-
-        # optimizer.load_state_dict(checkpoint['optimizer'])
-        # logger.info("=> loaded checkpoint '{}' (epoch {})".format(
-        #     checkpoint_file, checkpoint['epoch']))
-
-    # milestones = opts.lr_dec_epoch
-    # lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
-    #     optimizer, milestones, opts.lr_dec_factor,
-    #     last_epoch=last_epoch
-    # )  # scheduler will be set to place given last from checkpoints
-    # if opts.epoch_step > 0:
-    #     end_epoch = min(opts.end_epoch, opts.start_epoch + opts.epoch_step)
-    # else:
-    #     end_epoch = opts.end_epoch
-
-    ##################################################################################### not sure what this is
-    # dump_input = torch.rand(
-    #     (1, opts.input_nc, opts.sz_pch[1], opts.sz_pch[0])
-    # )
-    # logger.info(get_model_summary(model, dump_input))
-    ##################################################################################### not sure what this is
-
-
-    model = torch.nn.DataParallel(model, device_ids=opts.gpu_ids).cpu() # load the model??? might not be needed
 
     n_iter = opts.trainIter  # only for test purpose     quick test
-    # if not if_test:
-    #     for epoch in range(begin_epoch,end_epoch):
-    #         if opts.display_id > 0:
-    #             visualizer.reset()      # clean up the vis
-    #         # train for one epoch
-    #         rst_trn = train(train_loader, SLP_rd_train, model, criterion, optimizer, epoch, n_iter=n_iter, logger=logger, opts=opts, visualizer=visualizer)
-    #         losses += rst_trn['losses']
-    #         accs += rst_trn['accs']
-
-    #         # evaluate on validation set    to update
-    #         rst_test = validate(
-    #             test_loader, SLP_rd_test, model, criterion,
-    #             n_iter=n_iter, logger=logger, opts=opts)   # save preds, gt, preds_in ori, idst_normed to recovery, error here for last epoch?
-    #         pck_all = rst_test['pck']
-    #         perf_indicator = pck_all[-1][-1] # the last entry
-    #         pckh05 = np.array(pck_all)[:, -1]   # the last indicies     15 x 11 last
-    #         titles_c = list(SLP_rd_test.joints_name[:SLP_rd_test.joint_num_ori]) + ['total']
-    #         ut.prt_rst([pckh05], titles_c, ['pckh0.5'], fn_prt=logger.info)
-
-    #         lr_scheduler.step()     # new version updating here
-    #         if perf_indicator >= best_perf:
-    #             best_perf = perf_indicator
-    #             best_model = True
-    #         else:
-    #             best_model = False
-
-    #         logger.info('=> saving checkpoint to {}'.format(opts.model_dir))
-    #         ckp = {
-    #             'epoch': epoch + 1,     # epoch to next, after finish 0 this is 1
-    #             'model': opts.model,
-    #             'state_dict': model.module.state_dict(),
-    #             'best_state_dict': model.module.state_dict(),
-    #             'perf': perf_indicator,
-    #             'optimizer': optimizer.state_dict(),
-    #             'losses': losses,       # for later updating
-    #             'accs': accs,
-    #         }
-    #         torch.save(ckp, os.path.join(opts.model_dir, 'checkpoint.pth'))
-    #         if best_model:
-    #             torch.save(ckp, os.path.join(opts.model_dir, 'model_best.pth'))
-    #         # save directly, if statebest save another
-
-    #     final_model_state_file = os.path.join(
-    #         opts.model_dir, 'final_state.pth'     # only after last iters
-    #     )
-    #     logger.info('=> saving final model state to {}'.format(
-    #         final_model_state_file)
-    #     )
-    #     torch.save(model.module.state_dict(), final_model_state_file)
 
     # single test with loaded model, save the result
     logger.info('----run final test----')
